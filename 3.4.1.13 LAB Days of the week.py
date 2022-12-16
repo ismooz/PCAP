6da7fa18261c758feec3f3cb798b6e82e5e80a82
ismooz <69241713+ismooz@users.noreply.github.com>
2022-12-16 15:38:53 +0100

    Add via upload

---
+++ 3.4.1.13 LAB Days of the week.py
@@ -0,0 +1,36 @@
+﻿class WeekDayError(Exception):
+    pass
+	
+
+class Weeker:
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    day = []
+
+    def __init__(self, day):
+        if day not in self.days:
+            raise WeekDayError("Invalid day")
+        self.day = day
+
+    def __str__(self):
+        return self.day
+
+    def add_days(self, n):
+        index = self.days.index(self.day)
+        index = (index + n) % 7
+        self.day = self.days[index]
+
+    def subtract_days(self, n):
+        index = self.days.index(self.day)
+        index = (index - n) % 7
+        self.day = self.days[index]
+
+try:
+    weekday = Weeker('Mon')
+    print(weekday)
+    weekday.add_days(15)
+    print(weekday)
+    weekday.subtract_days(23)
+    print(weekday)
+    weekday = Weeker('Monday')
+except WeekDayError:
+    print("Sorry, I can't serve your request.")
