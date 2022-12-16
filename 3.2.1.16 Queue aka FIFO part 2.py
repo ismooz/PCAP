805304c8996f792261e4886954137cf77c81680c
ismooz <69241713+ismooz@users.noreply.github.com>
2022-12-16 10:05:16 +0100

    Add via upload

---
+++ 3.2.1.16 Queue aka FIFO part 2.py
@@ -0,0 +1,38 @@
+﻿class QueueError(IndexError):
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def put(self,elem):
+        self.queue.insert(0,elem)
+    def get(self):
+        if len(self.queue) > 0:
+            elem = self.queue[-1]
+            del self.queue[-1]
+            return elem
+        else:
+            raise QueueError
+
+
+class SuperQueue(Queue):
+    def __init__(self):
+        # Appelons la méthode __init__ de la classe parente
+        # pour initialiser la liste queue
+        super().__init__()
+
+    def isempty(self):
+        # Retournons True si la liste queue est vide, False sinon
+        return len(self.queue) == 0
+
+
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Queue empty")
