9f01158a92d733486e526dcdc36765e052be4eb5
ismooz <69241713+ismooz@users.noreply.github.com>
2022-12-16 16:12:39 +0100

    Add via upload

---
+++ 3.4.1.14 Points on a plane.py
@@ -0,0 +1,29 @@
+﻿import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = x
+        self.__y = y
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        dist_x = x - self.__x
+        dist_y = y - self.__y
+        return math.hypot(dist_x, dist_y)
+
+    def distance_from_point(self, point):
+        dist_x = point.getx() - self.__x
+        dist_y = point.gety() - self.__y
+        return math.hypot(dist_x, dist_y)
+
+
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
