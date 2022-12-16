878426bdbfe313980f7c987fb0d1a11920388b95
ismooz <69241713+ismooz@users.noreply.github.com>
2022-12-16 09:37:04 +0100

    Add file via upload

---
+++ 3.2.1.15 Queue aka FIFO.py
@@ -0,0 +1,28 @@
+﻿class QueueError(Exception):  # Choose base class for the new exception.
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.elements = []
+
+    def put(self, elem):
+        self.elements.insert(0, elem)
+
+    def get(self):
+        if len(self.elements) == 0:
+            raise QueueError()
+        else:
+            return self.elements.pop()
+                
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except QueueError:
+    print("Queue error")
