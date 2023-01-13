be8ef8d6a00ff10675b3e8d672bc11258dbdb745
ismooz <69241713+ismooz@users.noreply.github.com>
2023-01-13 10:48:41 +0100

    Add files via upload

---
+++ 4.5.1.22 LAB The datetime and time modules.py
@@ -0,0 +1,11 @@
+﻿from datetime import *
+
+date = datetime(2020, 11, 4, 14, 53, 0)
+
+print(date.strftime("%Y/%m/%d %H:%M:%S"))
+print(date.strftime("%Y/%B/%d %H:%M:%S %p"))
+print(date.strftime("%a, %Y %b %d"))
+print(date.strftime("%A, %Y %B %d"))
+print(date.strftime("Weekday: %w"))
+print(date.strftime("Day of the year: %j"))
+print(date.strftime("Week of the year: %W"))
